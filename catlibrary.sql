f78aadd3cb194519b5fe1115a3e1cde9e05628a8
babimagine <111130006+babimagine@users.noreply.github.com>
2022-11-14 14:59:59 -0300

    Update catlibrary.sql

--- catlibrary.sql
+++ catlibrary.sql
@@ -1,33 +1,37 @@
 CREATE TABLE LUGAR
 (
-    IDLUGAR     int             PRIMARY KEY,
+    IDLUGAR     var(20)                 ,
     ENDERECO    varchar2(50)    not null,
     NUMERO      int(10)         not null,
     BAIRRO      varchar(50)     not null,
     CEP         varchar(9)      not null,
     CIDADE      varchar(50)     not null,
     ESTADO      varchar2(50)    not null,
-    PAIS        varchar2(50)
+    PAIS        varchar2(50)    not null,
+    Constraint LUGAR_Id_PK   PRIMARY KEY(IDLUGAR)
 )
 
 CREATE TABLE CARTAO
 (
-    IDCARTAO                    PRIMARY KEY,
+    IDCARTAO    var(20)                 ,
     NUMERO      varchar(16)     not null,
     NOME        varchar(50)     not null,
     VENCIMENTO, int             not null,
-    SEGURANCA   int             not null
+    SEGURANCA   int             not null,
+    Constraint cartao_Id_PK  PRIMARY KEY(IDCARTAO)
 )
 
 CREATE TABLE CLIENTE
 (
-    IDCLIENTE  PRIMARY KEY,
+    IDCLIENTE   var(20)                 ,
     NOME        varchar(50)     not null,
     SOBRENOME   varchar(50)     not null,
     EMAIL       varchar(50)     not null,
     TELEFONE    int             not null,
     SENHA       varchar(32)     not null,
-    GENERO      varchar(1)		CHECK(TIPO IN ('M','F','O', 'N')),
+    GENERO      varchar(1)		Constraint CLIENTE_GENERO_CK CHECK(Sexo IN ('M','F','O', 'N')),
+    Constraint cliente_Id_PK  PRIMARY KEY(IDCLIENTE),
+    
     IDLUGAR     FOREIGN KEY,
     IDCARTAO    FOREIGN KEY,
     FOREIGN KEY(IDLUGAR) REFERENCES LUGAR (IDLUGAR),
@@ -36,15 +40,16 @@ CREATE TABLE CLIENTE
 
 CREATE TABLE LIVRO
 (
-    IDLIVRO PRIMARY KEY,
-    NOME        varchar(50)        not null,
-    DESCRICAO   varchar(500)       not null,
-    IMG         varchar(50)        not null,
-    EDITOR      varchar(32)        not null,
-    AUTOR       varchar(32)        not null,
-    LANCAMENTO  date               not null,
-    PRECO       float(7,2)         not null,
-    GENERO      varchar(32)        not null
+    IDLIVRO         var(20)                      ,
+    NOME            varchar2(50)         not null,
+    DESCRICAO       varchar2(500)        not null,
+    IMG             varchar2(50)         not null,
+    EDITOR          varchar2(32)         not null,
+    AUTOR           varchar2(32)         not null,
+    LANCAMENTO      varchar2(32)         not null,
+    PRECO           number(7,2)          not null,
+    GENERO          varchar2(32)         not null,
+    Constraint livro_Id_PK  PRIMARY KEY(ID LIVRO)
 )
 
         CREATE TABLE CLIENTE_LIVRO
